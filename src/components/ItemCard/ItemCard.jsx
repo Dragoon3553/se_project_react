13774f848d237ca94ec62a0c05507b2fea43be1e
Drew Woods <c.woodsiii@gmail.com>
2026-01-19 21:20:24 -0800

    Add syles and code for Footer, Main, and ItemCard

---
+++ src/components/ItemCard/ItemCard.jsx
@@ -0,0 +1,12 @@
+import "./ItemCard.css";
+
+function ItemCard({ item }) {
+  return (
+    <li className="card">
+      <h1 className="card__name">{item.name}</h1>
+      <img className="card__img" src={item.link} alt={item.name} />
+    </li>
+  );
+}
+
+export default ItemCard;
