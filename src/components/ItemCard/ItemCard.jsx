a30ba7eeda263f1a5b2230ca58e5cd3d923f87de
Drew Woods <c.woodsiii@gmail.com>
2026-05-25 23:28:54 -0700

    Add functionality for like button

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -2,31 +2,35 @@ import { useContext } from "react";
 
 // Context Import
 import LoginContext from "../../contexts/LoginContext";
+import CurrentUserContext from "../../contexts/CurrentUserContext";
 
-import "./ItemCard.css";
 import likeDefault from "../../assets/like_default.png";
 import likeActive from "../../assets/like_active.png";
+import "./ItemCard.css";
 
 function ItemCard({ item, onCardClick, onCardLike }) {
+  // Contexts
   const { isLoggedIn } = useContext(LoginContext);
+  const { currentUser } = useContext(CurrentUserContext);
+
+  const isLiked = item.likes.some((id) => id === currentUser._id);
 
   const handleCardClick = () => {
     onCardClick(item);
   };
 
   const handleLike = () => {
-    onCardLike(item);
+    onCardLike({ id: item._id, isLiked });
   };
-
   return (
     <li className="card">
       <div className="card__content">
         <h2 className="card__name">{item.name}</h2>
         {isLoggedIn && (
-          <button type="button" className="card__like-btn">
+          <button onClick={handleLike} type="button" className="card__like-btn">
             <img
-              src={likeDefault}
-              alt={`isLiked ? 'like active' : 'like default'`}
+              src={isLiked ? likeActive : likeDefault}
+              alt={isLiked ? "Item liked" : "Item not liked"}
               className="card__like-btn_img"
             />
           </button>
